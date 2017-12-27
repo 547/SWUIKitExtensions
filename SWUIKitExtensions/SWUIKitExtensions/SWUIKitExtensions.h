5aeb9ddd5afc86d80f4c993b7b6752b032369244
qiwang <timer_sevenwang@163.com>
2017-12-27 15:30:40 +0800

    create

---
+++ SWUIKitExtensions/SWUIKitExtensions/SWUIKitExtensions.h
@@ -0,0 +1,19 @@
+//
+//  SWUIKitExtensions.h
+//  SWUIKitExtensions
+//
+//  Created by seven on 2017/12/27.
+//  Copyright © 2017年 seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWUIKitExtensions.
+FOUNDATION_EXPORT double SWUIKitExtensionsVersionNumber;
+
+//! Project version string for SWUIKitExtensions.
+FOUNDATION_EXPORT const unsigned char SWUIKitExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWUIKitExtensions/PublicHeader.h>
+
+
